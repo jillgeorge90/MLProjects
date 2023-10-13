224388be8b4ee223f609c78eaef31b8e42b47b5f
Jill <jillgeorge18@gmail.com>
2023-10-14 00:40:26 +0530

    logging and error

---
+++ src/exception.py
@@ -0,0 +1,20 @@
+import sys
+import logging
+
+def errorMessageDetail(error,errorDetail:sys):
+    _,_,exc_tb = errorDetail.exc_info()
+    fileName = exc_tb.tb_frame.f_code.co_filename
+    errorMessage = "Error occured in Python script name[{0}],line number[{1}],error message[{2}]".format(
+        fileName,exc_tb.tb_lineno,str(error)
+    )
+    return errorMessage
+
+class CustomException(Exception):
+    def __init__(self,errorMessage,errorDetail:sys):
+        super().__init__(errorMessage)
+        self.errorMessage = errorMessageDetail(errorMessage,errorDetail=errorDetail)
+
+    def __str__(self):
+        return self.errorMessage    
+    
+
